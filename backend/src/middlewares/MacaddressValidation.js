44896131c780c9f6248bed48157a0c405d6b85fb
cesar <ocesaralmeida@gmail.com>
2021-11-24 18:59:03 -0300

    added .gitignore

---
+++ backend/src/middlewares/MacaddressValidation.js
@@ -0,0 +1,8 @@
+const MacaddressValidation = (req, res, next) =>{
+    if(!req.body.macaddress)
+        return res.status(400).json({ error: 'macaddress é obrigatório'});
+    else
+        next();
+};
+
+module.exports = MacaddressValidation;
