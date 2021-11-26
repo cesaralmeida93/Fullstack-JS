7a9bdad91cd989adc153970d49eba0b3861b6763
cesar <ocesaralmeida@gmail.com>
2021-11-26 04:49:25 -0300

    Criado Footer com Styled Components

--- web/web/src/views/Home/index.js
+++ web/web/src/views/Home/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import * as S from './styles'
 
 //NOSSOS COMPONENTES
-import Header  from "../../components/Header";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 function Home() {
   return (
     <S.Container>
       <Header/>
+      <Footer/>
     </S.Container>
   )
 }
