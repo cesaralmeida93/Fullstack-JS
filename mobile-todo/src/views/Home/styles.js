cc6037795138da29a86cc453007a2b5f1001a479
Cesar <ocesaralmeida@gmail.com>
2022-05-20 08:19:45 -0300

    added footer and filters

--- mobile-todo/src/views/Home/styles.js
+++ mobile-todo/src/views/Home/styles.js
@@ -6,6 +6,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'flex-start'
+  },
+  filter: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around',
+    height: 70,
+    alignItems: 'center'
+  },
+  filterTextActive: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#EE6B26'
+  },
+  filterTextInactive: {
+    color: '#20295F',
+    fontWeight: 'bold',
+    fontSize: 18,
+    opacity: 0.5
   }
 })
 
